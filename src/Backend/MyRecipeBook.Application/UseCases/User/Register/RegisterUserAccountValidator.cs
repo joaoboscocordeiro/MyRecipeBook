9b578aee8a5a2c22c76cf63496874c7db163da86
João Bosco Cordeiro de Lima Júnior <ti.junior@gmail.com>
2026-06-10 16:54:40 -0300

    Criando Required

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exception;
 
 namespace MyRecipeBook.Application.UseCases.User.Register;
 
@@ -7,7 +8,7 @@ public class RegisterUserAccountValidator : AbstractValidator<RequestRegisterUse
 {
     public RegisterUserAccountValidator()
     {
-        RuleFor(user => user.Name).NotEmpty().WithMessage("O nome não pode ser vazio!");
+        RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMesageException.VALIDATE_NAME_REQUIRED);
         RuleFor(user => user.Email).NotEmpty().WithMessage("O email não pode ser vazio!");
         RuleFor(user => user.Password).NotEmpty().WithMessage("A senha não pode ser vazia!");
         When(user => string.IsNullOrWhiteSpace(user.Email) == false, () =>
