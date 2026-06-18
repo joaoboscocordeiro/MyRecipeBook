2dd98cc682f901e98ca008c50a2c98ea80c24fde
João Bosco Júnior <ti.junior@gmail.com>
2026-06-18 18:28:08 -0300

    Salvando usuário no banco

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using MyRecipeBook.Domain.Repositories;
+
+namespace MyRecipeBook.Infrastructure.DataAccess;
+
+internal class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UnitOfWork(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Commit() => await _dbContext.SaveChangesAsync();
+}
