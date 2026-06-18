c87d75788339c72477fb58a152e67df2ca90006c
João Bosco Júnior <ti.junior@gmail.com>
2026-06-18 14:33:42 -0300

    Instalação e configuração de Entity Framework

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Infrastructure.DataAccess;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options){}
+
+    public DbSet<User> Users { get; set; }
+}
