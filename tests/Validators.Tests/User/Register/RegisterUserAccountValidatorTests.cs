2649afdb93197f3242bd6382e92ebf584302592a
João Bosco Júnior <ti.junior@gmail.com>
2026-06-29 19:54:42 -0300

    tests de empty

--- tests/Validators.Tests/User/Register/RegisterUserAccountValidatorTests.cs
+++ tests/Validators.Tests/User/Register/RegisterUserAccountValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using CommonTestUtilities.Requests;
 using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exception;
 using Shouldly;
 
 namespace Validators.Tests.User.Register;
@@ -21,5 +22,82 @@ public class RegisterUserAccountValidatorTests
 
         // Assert
         result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenNameIsEmpty()
+    {
+        // AAA => Arranger, Act, Assert
+
+        // Arrange
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourseMesageException.VALIDATE_NAME_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenEmailIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Email = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourseMesageException.VALIDATE_EMAIL_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenPasswordIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Password = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourseMesageException.VALIDATE_PASSWORD_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenEmailIsInvalid()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Email = "jr.com";
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourseMesageException.VALIDATE_EMAIL_INVALID));
+        });
     }
 }
