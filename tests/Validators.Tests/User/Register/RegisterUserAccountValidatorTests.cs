181b037a7f1928b571c09159fcf901553028b740
João Bosco Júnior <ti.junior@gmail.com>
2026-06-29 19:19:47 -0300

    Criando tests

---
+++ tests/Validators.Tests/User/Register/RegisterUserAccountValidatorTests.cs
@@ -0,0 +1,25 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Register;
+using Shouldly;
+
+namespace Validators.Tests.User.Register;
+
+public class RegisterUserAccountValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        // AAA => Arranger, Act, Assert
+
+        // Arrange
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+
+        var validator = new RegisterUserAccountValidator();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        result.IsValid.ShouldBeTrue();
+    }
+}
